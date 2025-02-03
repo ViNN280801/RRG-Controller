0140b9a2f49ece3c0c855e187fc0fce7848649d0
ViNN280801 <vladislav_semykin01@mail.ru>
2025-02-03 10:10:41 +0300

    Added copying shared lib to the root dir and copying C test file for the real device to the root dir
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ make
 
 cd ..
 cp -v build/c_api/src/librrg.so ui/resources/librrg.so
+cp -v build/c_api/src/librrg.so ./librrg.so
+cp -v build/c_api/rrg_test ./rrg_test
 
-rm -rf build/*
+rm -rf build/
