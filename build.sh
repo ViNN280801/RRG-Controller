f252af6669a223116b5ee7dbb8b8ad3ece8df7d0
ViNN280801 <vladislav_semykin01@mail.ru>
2025-02-03 16:33:56 +0300

    Added helper path variables
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
+LIBRRG_PATH="build/c_api/src/rrg/librrg.so"
+LIBRRG_RESOURCES_PATH="ui/resources/librrg.so"
+LIBRELAY_PATH="build/c_api/src/relay/librelay.so"
+LIBRELAY_RESOURCES_PATH="ui/resources/librelay.so"
+
+RRG_LIVE_TEST_PATH="build/c_api/rrg_live_test"
+RELAY_LIVE_TEST_PATH="build/c_api/relay_live_test"
+
 mkdir -pv build
 cd build
-cmake ..
-make
+cmake .. || exit 1
+make || exit 1
 
 cd ..
-cp -v build/c_api/src/librrg.so ui/resources/librrg.so
-cp -v build/c_api/src/librrg.so ./librrg.so
-cp -v build/c_api/rrg_test ./rrg_test
+cp -v $LIBRRG_PATH $LIBRRG_RESOURCES_PATH
+cp -v $LIBRRG_PATH ./librrg.so
+cp -v $RRG_LIVE_TEST_PATH ./rrg_live_test
 
-# rm -rf build/
+rm -rf build/
